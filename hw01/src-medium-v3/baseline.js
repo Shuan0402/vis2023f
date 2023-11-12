7696821d37db8b1f6ee967c6ad57cb0e1e967711
Shuan0402 <st980155@gmail.com>
2023-11-12 12:23:05 +0800

    test v3

--- hw01/src-medium-v3/baseline.js
+++ hw01/src-medium-v3/baseline.js
@@ -1,126 +1,62 @@
-// 生成隨機班級的函數
-function getRandomClass(){
-    const department = ['資工系', '資工所', '電資AI', '電資資安', '創新AI'];
-    return department[Math.floor(Math.random() * department.length)];
+function createAppleLevel() {
+    d3.select("#apple_level")
+        .append("table")
+        .append("tr")
+        .selectAll("td")
+        .data([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
+        .enter()
+        .append("td")
+        .text(function (d, i) {
+            if (i <= 10 || isNaN(d)) return d;
+        })
+        .append("img")
+        .attr("src", function (d, i) {
+            if (d >= 0 && d <= 10) return `./img/${d}.svg`;
+            else return "./svg10/00.svg";
+        })
+        .attr("x", "0")
+        .attr("y", "0")
+        .attr("width", function (d, i) { return (i + 1) * 60; })
+        .attr("height", function (d, i) { return (i + 1) * 60; });
 }
 
-// 生成隨機學號的函數
-function getRandomStudentID() {
-    const year = ['111', '112'];
-    const departmentId = ['590', '598', 'C52', 'C53', 'C71'];
-    const randomYear = year[Math.round(Math.random() * (year.length - 1))];
-    const randomDepartmentId = departmentId[Math.round(Math.random() * (departmentId.length - 1))];
-    const num = Math.round(Math.random() * 1000);
-    const id = randomYear + randomDepartmentId + num.toString();
-    return id;
+// 將資料處理的部分包裝成一個函式
+function createTranscripts(data) {
+
+    parsedCSV = d3.csv.parseRows(data);
+
+    var container = d3.select("Transcripts")
+        .append("table")
+        .selectAll("tr")
+        .data(parsedCSV)
+        .enter()
+        .append("tr")
+        .selectAll("td")
+        .data(function (d) { return d; }).enter()
+        .append("td")
+        .html(function (d, i) {
+            if (i === 4 && d !== 'GitHub 帳號') {
+                return '<a href="https://' + d + '.github.io/vis2023f/" target="_blank"><img src="https://' + d + '.github.io/vis2023f/hw00/me.jpg"></a>' + '<hr><a href="https://github.com/' + d + '/vis2023f/" target="_blank">' + d + '</a>';
+            } else if (i === 0 || i === 2 || isNaN(d)) {
+                return d;
+            }
+        })
+        .filter(function (d, i) { return (i > 2 && !isNaN(d) && d !== ""); })
+        .append("img")
+        .attr("src", function (d, i) {
+            if (d >= 0 && d <= 10) {
+                return `./img/${d}.svg`;
+            } else {
+                return "./svg10/00.svg";
+            }
+        })
+        .attr("width", 50)
+        .attr("height", 50);
 }
 
-// 生成隨機姓名的函數
-function getRandomName() {
-    const name_start = 0x4E00;
-    const name_end = 0x9FFF;
-    let name = '';
-    for (let i = 0; i < 3; i++) {
-        const randomUnicode = Math.floor(Math.random() * (name_end - name_start + 1) + name_start);
-        name += String.fromCharCode(randomUnicode);
-    }
-
-    return name;
-}
-
-// 生成隨機 github 帳號
-function generateRandomGithub() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    
-    let result = '';
-
-    for (let i = 0; i < 10; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        result += characters.charAt(randomIndex);
-    }
-
-    return result;
-}
-
-// 生成隨機成績的函數
-function getRandomScore() {
-    let scores = '';
-    for (let i = 0; i < 9; i++) {
-        scores += Math.floor(Math.random() * 101).toString() + ',';
-    }
-    scores += Math.floor(Math.random() * 101).toString();
-    return scores; // 成績範圍在0到100之間
-}
-
-// 生成虛擬成績單的函數
-function generateTranscript(num) {
-    const transcript = [];
-    for (let i = 0; i < num; i++) {
-        const student = {
-            no: i + 1,
-            class: getRandomClass(),
-            studentID: getRandomStudentID(),
-            name: getRandomName(),
-            githubAccount: generateRandomGithub(),
-            score: getRandomScore(),
-        };
-        transcript.push(student);
-    }
-    return transcript;
-}
-
-// 建立成績的 CSV 檔案
-function createTranscripCSV(){
-    let csvContent = '序號,班級,學號,姓名,Github 帳號,作業一,作業二,作業三,作業四,作業五,作業六,作業七,作業八,作業九,作業十\n';
-
-    let fakeTranscript = generateTranscript(120);
-
-    fakeTranscript.forEach((student) => {
-        csvContent += `${student.no},${student.class},${student.studentID},${student.name},${student.githubAccount},${student.score}\n`;
-    });
-
-    return csvContent;
-}
-
-// HTML 開啟時會執行以下操作
-document.addEventListener('DOMContentLoaded', function() {
-    // 建立成績的 CSV 檔案
-    csvContent = createTranscripCSV();
-
-    // 將 CSV 的內容填入 HTML 的 Table 中
-    const table = document.getElementById('scoreTable');
-    const rows = csvContent.split('\n');
-
-    rows.forEach((row, index) => {
-        if (index !== rows.length - 1 || row.trim().length > 0){
-            const cells = row.split(',');
-            const tr = document.createElement('tr');
-            
-            cells.forEach(cell => {
-                const td = document.createElement('td');
-                td.textContent = cell;
-                tr.appendChild(td);
-            });
-
-            table.appendChild(tr);
-        }
-    });
-
-    document.body.appendChild(table);
-    
-    // 按下 download 按鈕
-    const downloadButton = document.getElementById('downloadButton');
-    
-    downloadButton.addEventListener('click', function() {
-        const blob = new Blob(['\uFEFF' + csvContent], { type: 'text/csv;charset=utf-8;' });
-        const url = URL.createObjectURL(blob);
-
-        const link = document.createElement('a');
-        link.setAttribute('href', url);
-        link.setAttribute('download', 'output.csv');
-
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+document.addEventListener("DOMContentLoaded", function() {
+    createAppleLevel();
+    d3.text("./output.csv", function (data) {
+        createTranscripts(data);
     });
 });
