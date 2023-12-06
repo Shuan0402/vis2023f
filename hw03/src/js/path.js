78ff8ce9128302313807858fcdecec175f79c2f0
Shuan0402 <st980155@gmail.com>
2023-12-06 16:27:34 +0800

    update hw3 js

--- hw03/src/js/path.js
+++ hw03/src/js/path.js
@@ -1,41 +1,3 @@
-/*const icon = document.getElementById('icon');
-const jsonUrl = '../json/userLocationData.json'; 
-
-function calculatePosition(x, y) {
-  const minX = 10.45;
-  const maxX = 60.47;
-  const minY = -11.64;
-  const maxY = 15.91;
-
-  const iconX = ((x - minX) / (maxX - minX)) * 100;
-  const iconY = ((y - minY) / (maxY - minY)) * 100;
-
-  return { x: iconX, y: iconY };
-}
-
-function updateIconPosition(x, y) {
-  const { x: iconX, y: iconY } = calculatePosition(x, y);
-  icon.style.left = `${iconX}%`;
-  icon.style.top = `${iconY}%`;
-}
-
-function loadjson() {
-  fetch(jsonUrl)
-    .then(response => response.json())
-    .then(data => {
-      // Assuming the data is an array of objects
-      const lastDataPoint = data[data.length - 1];
-      updateIconPosition(lastDataPoint.X, lastDataPoint.Y);
-    })
-    .catch(error => console.error('Error fetching JSON:', error));
-}
-
-// 初始加載icon位置
-loadjson();
-
-// 每秒更新一次位置
-setInterval(loadjson, 1000);
-*/
 const icon = document.getElementById('icon');
 const jsonUrl = '../json/userLocationData.json';
 let currentIndex = 0;
@@ -61,28 +23,34 @@ function updateIconPosition(x, y) {
 }
 
 function loadjson() {
-  fetch(jsonUrl)
-    .then(response => response.json())
-    .then(data => {
-      if (data && data.length > 0) {
-        const point = data[currentIndex];
+    fetch(jsonUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (!Array.isArray(data) || data.length === 0) {
+                throw new Error('Invalid or empty data received');
+            }
 
-        if (point.Floor !== currentFloor) {
-          // 換樓層時的處理，這裡可以添加你的樓層切換相關邏輯
-          console.log(`Switched to Floor ${point.Floor}`);
-          currentFloor = point.Floor;
-        }
+            const point = data[currentIndex];
 
-        updateIconPosition(point.X, point.Y);
-        currentIndex++;
+            if (point.Floor !== currentFloor) {
+                console.log(`Switched to Floor ${point.Floor}`);
+                currentFloor = point.Floor;
+                // 在此處執行樓層相關操作，例如更新地圖顯示
+            }
 
-        // 如果資料到達末尾，重新從頭開始
-        if (currentIndex >= data.length) {
-          currentIndex = 0;
-        }
-      }
-    })
-    .catch(error => console.error('Error fetching or parsing JSON:', error));
+            updateIconPosition(point.X, point.Y);
+            currentIndex++;
+
+            if (currentIndex >= data.length) {
+                currentIndex = 0;
+            }
+        })
+        .catch(error => console.error('Error fetching or parsing JSON:', error));
 }
 
 // 初始加載 icon 位置
