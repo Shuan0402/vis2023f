5f56da8465a2f656f364193da857b138ef6af530
Shuan0402 <st980155@gmail.com>
2023-12-07 08:37:14 +0800

    update hw3 trace.js/png

--- hw03/src/js/trace.js
+++ hw03/src/js/trace.js
@@ -5,53 +5,60 @@ let currentIndex = 0;
 let currentFloor = '1F'; // 新增 currentFloor 變數
 
 function calculatePosition(x, y) {
-  const minX = 10.45;
-  const maxX = 60.47;
-  const minY = -11.64;
-  const maxY = 15.91;
+    const minX = 10.45;
+    const maxX = 60.47;
+    const minY = -11.64;
+    const maxY = 15.91;
 
-  const iconX = ((x - minX) / (maxX - minX)) * 100;
-  const iconY = ((y - minY) / (maxY - minY)) * 100;
+    const iconX = ((x - minX) / (maxX - minX)) * 100;
+    const iconY = ((y - minY) / (maxY - minY)) * 100;
 
-  return { x: iconX, y: iconY };
+    return { x: iconX, y: iconY };
 }
 
 function updateIconPosition(x, y) {
-  const { x: iconX, y: iconY } = calculatePosition(x, y);
-  icon.style.left = `${iconX}%`;
-  icon.style.top = `${iconY}%`;
+    const { x: iconX, y: iconY } = calculatePosition(x, y);
+    icon.style.left = `${iconX}%`;
+    icon.style.top = `${iconY}%`;
 
-  // 繪製移動痕跡
-  const tracePoint = document.createElement('div');
-  tracePoint.className = 'trace-point';
-  tracePoint.style.left = `${iconX}%`;
-  tracePoint.style.top = `${iconY}%`;
-  traceContainer.appendChild(tracePoint);
+    // 繪製移動痕跡
+    const tracePoint = document.createElement('div');
+    tracePoint.className = 'trace-point';
+    tracePoint.style.left = `${iconX}%`;
+    tracePoint.style.top = `${iconY}%`;
+    traceContainer.appendChild(tracePoint);
 }
 
+
 function loadjson() {
-  fetch(jsonUrl)
-    .then(response => response.json())
-    .then(data => {
-      if (data && data.length > 0) {
-        const point = data[currentIndex];
+    fetch(jsonUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (!Array.isArray(data) || data.length === 0) {
+                throw new Error('Invalid or empty data received');
+            }
 
-        if (point.Floor !== currentFloor) {
-          // 換樓層時的處理，這裡可以添加你的樓層切換相關邏輯
-          console.log(`Switched to Floor ${point.Floor}`);
-          currentFloor = point.Floor;
-        }
+            const point = data[currentIndex];
 
-        updateIconPosition(point.X, point.Y);
-        currentIndex++;
+            if (point.Floor !== currentFloor) {
+                console.log(`Switched to Floor ${point.Floor}`);
+                currentFloor = point.Floor;
+                switchFloorLogic(currentFloor);
+            }
 
-        // 如果資料到達末尾，重新從頭開始
-        if (currentIndex >= data.length) {
-          currentIndex = 0;
-        }
-      }
-    })
-    .catch(error => console.error('Error fetching or parsing JSON:', error));
+            updateIconPosition(point.X, point.Y);
+            currentIndex++;
+
+            if (currentIndex >= data.length) {
+                currentIndex = 0;
+            }
+        })
+        .catch(error => console.error('Error fetching or parsing JSON:', error));
 }
 
 // 初始加載 icon 位置
